e9c8a380686074e6e77f085e42de1fe72ee80aa5
Bernard Gaitho <gaithoben@gmail.com>
2022-11-03 11:58:51 +0300

    package 44 add secure-store

--- lib/index.ts
+++ lib/index.ts
@@ -66,5 +66,4 @@ export { default as toastr } from './Toastr';
 
 // Async Storage
 export { default as AsyncStorage } from './storage/AsyncStorage';
-export { default as SecureStore } from './storage/SecureStore';
 // export { default as EncryptedStorage } from './storage/EncryptedStorage';
