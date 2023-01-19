121942418210bb24f73570d3d5ad83638dc98af9
Bernard Gaitho <gaithoben@gmail.com>
2023-01-19 08:14:10 +0300

    add react-native-svg-transformer

--- lib/modal/ActionModal.tsx
+++ lib/modal/ActionModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, ViewStyle } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import Block from '../Block';
 import Text from '../Text';
@@ -8,45 +8,30 @@ import metrics from '../theme/Metrics';
 
 const android = Platform.OS === 'android';
 
-const ActionModal = ({ children, containerStyle = {}, props }) => {
-  const renderContent = () => (
-    <Block
-      style={{
-        backgroundColor: 'white',
-        padding: sizes.padding,
-        borderRadius: 10,
-        height: metrics.height * 0.8,
-
-        ...containerStyle,
-      }}
-      shadow
-      flex={false}
-    >
-      <Block flex={false} row center top style={{ marginTop: -8 }}>
-        <Text size={32} style={{ lineHeight: 5 }} color="#555">
-          __
-        </Text>
-      </Block>
-      {children}
-    </Block>
-  );
-
+const ActionModal = ({
+  children,
+  containerStyle = {},
+  showHandle = false,
+  props,
+}: {
+  children: any;
+  containerStyle?: ViewStyle;
+  showHandle?: boolean;
+  props?: typeof BottomSheet;
+}) => {
   const bottomSheetRef = React.useRef<BottomSheet>(null);
 
-  React.useEffect(() => {
-    if (bottomSheetRef.current) {
-      bottomSheetRef.current.snapToIndex(2);
-    }
-  }, [bottomSheetRef]);
+  const snapPoints = React.useMemo(() => [metrics.height * 0.8, 300, 150], []);
 
   return (
     <BottomSheet
       ref={bottomSheetRef}
-      snapPoints={[metrics.height * 0.8, 300, 100, 1]}
-      borderRadius={10}
-      renderContent={renderContent}
+      index={snapPoints.length - 1}
+      snapPoints={snapPoints}
       {...props}
-    />
+    >
+      <Block>{children}</Block>
+    </BottomSheet>
   );
 };
 
